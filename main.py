cda9754f981cb477a150547a1dc149a8da5e89f7
Mai Mohamed <179390147+MaiM0hamed@users.noreply.github.com>
2025-10-21 11:18:19 +0300

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-
-# Load environment variables from .env file
-from src.routes import base, data
-
-app = FastAPI()
-#funtion control what in and out from this function @
-app.include_router(base.base_router, prefix="/base")
-app.include_router(data.data_router, prefix="/data")
